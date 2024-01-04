90c7bc7a011d69e368172f5ec9dd6f7853eaf906
gayathripgayu <gayathrinair266@gmail.com>
2024-01-03 21:32:07 -0800

    header added

---
+++ test/src/header.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './Header.css';
+
+function Header() {
+  return (
+    
+  <div className="Header">    
+	<title>BizBook Directory Template by Rn53Themes.net</title>
+	{/* <!--== META TAGS ==--> */}
+	<meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+	<meta name="theme-color" content="#76cef1" />
+	<meta property="og:image" content="images/home/logo-b.png" />
+	<meta name="description" content="Kick start your online directory with BizBook Directory Template. Its comes with ultimate features like listings, events, blog, community. Try Bizbook directory now!"/>
+	<meta name="keyword" content="bizbook directory, bizbook directory template, business directory template, php directory template, functional php template, php listing template, php dirctory database, local listing php template"/>
+	{/* <!--== FAV ICON(BROWSER TAB ICON) ==--> */}
+	<link rel="shortcut icon" href="images/fav.ico" type="image/x-icon"/>
+	{/* <!--== GOOGLE FONTS ==--> */}
+	<link href="https://fonts.googleapis.com/css?family=Oswald:700|Source+Sans+Pro:300,400,600,700&display=swap" rel="stylesheet"/>
+	{/* <!--== WEB ICON FONTS ==--> */}
+	<link rel="preload" as="font" href="css/icon.woff2" type="font/woff2" crossorigin="anonymous"/>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+	{/* <!--== CSS FILES ==--> */}
+	<link rel="stylesheet" href="css/jquery-ui.css"/>
+	<link rel="stylesheet" href="css/bootstrap.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+	<link rel="stylesheet" href="css/fonts.css"/>
+	{/* <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]> */}
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+   {/*  <![endif]--> */}
+</div>
+
+    
+  );
+}
+
+export default Header;
